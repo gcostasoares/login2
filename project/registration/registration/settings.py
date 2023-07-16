6d8a8b69dcfc22f7d14938a35ae10a6757d69c0f
Gabriel Costa Soares <119671456+gcostasoares@users.noreply.github.com>
2023-07-16 17:38:46 +0200

    Update settings.py

--- project/registration/registration/settings.py
+++ project/registration/registration/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
+
 
 
 SECRET_KEY = '2_8@9$c!0-h9d30h5(16-um^&)f_s%f5+ymx*ak2b9m#$m^n=-'
@@ -79,10 +80,15 @@ WSGI_APPLICATION = 'registration.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        'NAME': os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        'USER': os.environ.get("SQL_USER", "user"),
+        'PASSWORD': os.environ.get("SQL_PASSWORD", "password"),
+        'HOST': os.environ.get("SQL_HOST", "localhost"),
+        'PORT': os.environ.get("SQL_PORT", "5432"),
     }
 }
+
 
 
 # Password validation

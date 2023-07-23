19fb0e61ae1db354b005fc982069488ac833ba0a
Gabriel Costa Soares <119671456+gcostasoares@users.noreply.github.com>
2023-07-23 11:48:57 +0200

    Update settings.py

--- project/registration/registration/settings.py
+++ project/registration/registration/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
 
 
 
@@ -73,6 +72,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'registration.wsgi.application'
+
 
 
 # Database
